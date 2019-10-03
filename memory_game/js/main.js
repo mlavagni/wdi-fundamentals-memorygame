d458893a48dae7e13d7b08e475b2d3c4cdb8aa50
mark <mark@marks-MacBook-Pro.local>
2019-10-03 15:54:38 -0500

    create game logic with functions

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -2,22 +2,34 @@
 
 var cards = ["queen", "queen", "king", "king"];
 
-var cardOne = cards[0];
-var cardTwo = cards[2];
+//var cardOne = cards[0];
+//var cardTwo = cards[2];
 
 var cardsInPlay = [];
 
-cardsInPlay.push(cardOne);
-cardsInPlay.push(cardTwo);
-
-
-console.log("user flipped " + cardsInPlay[0]);
-console.log("user flipped " + cardsInPlay[1]);
-
-if (cardsInPlay.length === 2){
+function checkForMatch() {
 	if (cardsInPlay[0]===cardsInPlay[1]){
-		alert("You found a match!");
-	} else {
-		alert("sorry try again");
+			alert("You found a match!");
+		} else {
+			alert("sorry try again");
+		}
+};
+
+
+function flipCard(cardId){
+	//cardsInPlay.push(cardOne);
+	//cardsInPlay.push(cardTwo);
+
+	//console.log("user flipped " + cardsInPlay[0]);
+	//console.log("user flipped " + cardsInPlay[1]);
+
+	console.log("user flipped " + cards[cardId]);
+	cardsInPlay.push(cards[cardId]);
+
+	if (cardsInPlay.length === 2){
+		checkForMatch();
 	}
-}
+};
+
+flipCard(0);
+flipCard(2);
